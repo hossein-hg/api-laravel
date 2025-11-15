37efe3088dbef075734abaeaa7212ff5eecd6fa5
hossein-hg <ghiasihossein2015@gmail.com>
2025-11-15 10:05:54 +0330

    spatie added to bootstrap/cache/packeges in local

--- app/Models/User.php
+++ app/Models/User.php
@@ -6,6 +6,7 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
+use Spatie\Permission\Traits\HasRoles;
 use Tymon\JWTAuth\Contracts\JWTSubject;
 class User extends Authenticatable implements JWTSubject
 {
@@ -24,7 +25,7 @@ class User extends Authenticatable implements JWTSubject
         'gender',
         'phone',
     ];
-
+    use HasRoles;
     /**
      * The attributes that should be hidden for serialization.
      *
